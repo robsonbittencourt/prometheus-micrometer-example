e91f0b85e2eead40e3bed82d84e8c8400441e742
robsonbittencourt <robson.luizv@gmail.com>
2021-03-28 23:31:10 -0300

    Add spring project structure

---
+++ src/main/java/com/rbittencourt/prometheus/micrometer/example/HelloController.java
@@ -0,0 +1,14 @@
+package com.rbittencourt.prometheus.micrometer.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello()  {
+        return "hello";
+    }
+
+}
---
+++ src/main/java/com/rbittencourt/prometheus/micrometer/example/PrometheusMicrometerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rbittencourt.prometheus.micrometer.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusMicrometerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusMicrometerExampleApplication.class, args);
+	}
+
+}
